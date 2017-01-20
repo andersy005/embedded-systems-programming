e4c21485c1e9524187196663cb6b8025a2b990ec
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-01-19 23:47:44 -0600

    :sparkles: Counting

---
+++ counting/main.c
@@ -0,0 +1,20 @@
+
+int main()
+{
+  int counter = 0;
+  
+  // increment the counter
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  
+  
+  unsigned int count = 0;
+  ++count;
+  ++count;
+  
+  
+  return 0;
+}
